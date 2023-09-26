2f1a6dfec4df394d3eed8f543205da260b82a92c
54-xenon <congas.rabbi_0b@icloud.com>
2023-09-26 20:39:37 +0900

    ヘッダーが完成

--- src/compornent/Header.jsx
+++ src/compornent/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import './CSS_component/Header.css'
+// CSSファイルをインポートする
 const Header = () => {
   return (
     <div>
+      <section>
+        {/* ヘッダーのせセクションです */}
+        <header className='header01'>
+          <h1 className='header01-logo'>PlantRecode</h1>
+          <nav className='header01-nav'>
+            <ul className='header01-list'>
+              <li className='header01-item'><a href="#">Home</a></li>
+              <li className='header01-item'><a href="#">Account</a></li>
+            </ul>
+          </nav>
+        </header>
+      </section>
+      <section>
+        {/* メインとなるセクション */}
         
+      </section>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/component/**/*.{js,jsx}','./src/**/*.{html,js,jsx}'],
+  // content: ['./src/component/**/*.{js,jsx}','./src/**/*.{html,js,jsx}'],
+  // 一旦はtailwindCSSをオフにして作ってみる
   theme: {
     extend: {},
   },
@@ -8,4 +9,4 @@ module.exports = {
 }
 
 // なんかtailwind.config.js(設定用のファイル)のcontent(適用する範囲を指定する)を変更すると良さそう
-// App.jsとcomponentフォルダを適用の範囲を指定すると改善
+// App.jsとcomponentフォルダを適用の範囲を指定すると改善する
